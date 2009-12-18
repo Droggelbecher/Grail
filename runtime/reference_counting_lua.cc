0a7a46da8426566e9a288b3b4527dbf80d2a230d
Henning Hasemann <henning@hasemail.de>
2009-12-18 15:26:37 +0100

    Onoz, forgot a file

---
+++ runtime/reference_counting_lua.cc
@@ -0,0 +1,26 @@
+
+#include "reference_counting_lua.h"
+
+template <typename T>
+void pushReference(lua_State* L, T* target) {
+  target->incrementeReferences();
+
+  void *ptr = (void*)(dynamic_cast<ReferenceCounted*>(target));
+  void *userdata = lua_newuserdata(L, sizeof(void*));
+  (*(void**)userdata) = ptr;
+
+  lua_newtable(L);
+  lua_pushcfunction(L, &onReferenceCollected);
+
+  lua_setmetatable(L, -2);
+}
+
+
+int onReferenceCollected(lua_State* L) {
+  // only parameter: a full userdata object
+  void *userdata = lua_touserdata(L, 1);
+  void *ptr = *((void**)userdata);
+  ((ReferenceCounted*)ptr)->decrementReferences();
+}
+
+
