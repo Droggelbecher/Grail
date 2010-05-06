f30d3c9ed490388592a4ec9012150a8e50bbc06d
BAM <>
2010-05-06 13:24:57 +0200

    first step of using alure/openal for audio, but no sound yet

--- lib/audio.cc
+++ lib/audio.cc
@@ -1,10 +1,12 @@
 // vim: set noexpandtab:
 
+#include <alure.h>
 #include "audio.h"
 #include "debug.h"
 #include "game.h"
 #include "sdl_exception.h"
 #include "resource_manager.h"
+
 
 namespace grail {
 
@@ -18,6 +20,15 @@ Audio::Audio() {
 }
 
 Audio::~Audio() {
+}
+
+Task::Ptr Audio::playSound(std::string resource, size_t loops, Audio::Volume v)
+{
+	ALuint src,buf;
+	alureInitDevice(NULL,NULL);
+	alGenSources(1, &src);
+	buf = alureCreateBufferFromFile(resource.c_str());
+	alSourcei(src, AL_BUFFER, buf);
 }
 
 
