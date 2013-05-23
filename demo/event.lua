23ecbac2343448caa7d09082c0afd04ca9516a88
Kasalehlia <kasalehlia@clonejo.de>
2013-05-23 14:06:10 +0200

    added event passing to the lua script

---
+++ demo/event.lua
@@ -0,0 +1,24 @@
+-- vim: set noexpandtab:
+
+module('event', package.seeall)
+
+listeners = {}
+
+function on(t, cb)
+    if listeners[t] == nil then
+        listeners[t] = {}
+    end
+    table.insert(listeners[t], cb)
+end
+
+function throw(t, data)
+    if listeners[t] ~= nil then
+        for i,v in pairs(listeners[t]) do
+            if v(data) then
+               table.remove(listeners[t], i)
+            end
+        end
+    end
+end
+
+GAME:setEventCallback(throw)
